d975f57fd0db2a31aeb3ac048b5798e9b7a6dfcf
gabriel <gcarlessox@gmail.com>
2023-08-29 22:51:31 -0300

    versao incial heatmap

---
+++ lib/controllers/heatmap_controller.dart
@@ -0,0 +1,7 @@
+import 'package:get/get.dart';
+import 'package:track_app/model/property.dart';
+
+class HeatmapController extends GetxController {
+  Property property;
+  HeatmapController(this.property);
+}
